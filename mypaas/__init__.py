fa57a1b8880753f70327ef460bc7c9f0fa4f5eb0
Almar Klein <almar.klein@gmail.com>
2020-12-02 10:16:52 +0100

    use latest traefik version

--- mypaas/__init__.py
+++ mypaas/__init__.py
@@ -8,7 +8,7 @@ automatic https (via Let's Encrypt) and deployments via dockerfiles.
 
 
 __version__ = "0.4.4"
-__traefik_version__ = "2.2.8"
+__traefik_version__ = "2.3.4"
 
 version_info = tuple(map(int, __version__.split(".")))
 
