c48e4efa7ba9c85dd9d514bf9ce81d15233933c8
Almar Klein <almar@almarklein.org>
2021-03-22 15:16:25 +0100

    bump version

--- mypaas/__init__.py
+++ mypaas/__init__.py
@@ -7,7 +7,7 @@ automatic https (via Let's Encrypt) and deployments via dockerfiles.
 # flake8: noqa
 
 
-__version__ = "0.5.2"
+__version__ = "0.5.3"
 __traefik_version__ = "2.4.7"
 
 version_info = tuple(map(int, __version__.split(".")))
