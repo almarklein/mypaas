f5e31ec346de61d4aa78fb4665c29ce2344bf20c
Lilah Abigail Ruth <72781354+lilahtovmoon@users.noreply.github.com>
2021-03-25 09:51:23 -0400

    pull the latest images for Dockerfile FROM declarations (#27)
    This is important if the deploy is using a pre-built image where you're using the "latest" tag and want it to fetch the latest version rather than re-deploying what it has.
    
    As the deploy docs note, a simple dockerfile like `FROM registry.example.com/your/image:tag` can let you deploy a pre-built image, but if you're using the "latest" tag, `docker build` won't re-fetch the image without `--pull`.
    
    https://github.com/almarklein/mypaas/blob/main/docs/deploying.md#pushing-a-deployment

--- mypaas/server/_deploy.py
+++ mypaas/server/_deploy.py
@@ -270,7 +270,7 @@ def _deploy_no_scale(container_infos, deploy_dir, service_name, prepared_cmd):
     time.sleep(1)
 
     yield "building image"
-    dockercall("build", "-t", image_name, deploy_dir)
+    dockercall("build", "--pull", "-t", image_name, deploy_dir)
 
     # There typically is one, but there may be more, if we had failed
     # deploys or if previously deployed with scale > 1
@@ -322,7 +322,7 @@ def _deploy_scale_safe(container_infos, deploy_dir, service_name, prepared_cmd, 
     time.sleep(1)
 
     yield "building image"
-    dockercall("build", "-t", image_name, deploy_dir)
+    dockercall("build", "--pull", "-t", image_name, deploy_dir)
 
     old_ids = get_id_name_for_this_service(container_infos)
     unique = str(int(time.time()))
